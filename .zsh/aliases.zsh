3cc3ed6b88e03502f94b43e284f6faad05f50d4d
Shimo888 <esdata0@gmail.com>
2025-11-18 09:33:03 +0900

    feat: create config alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,4 +1,7 @@
 # エイリアス設定
-alias ll='ls -l'             # 詳細情報表示
-alias la='ls -la'            # 隠しファイルも表示
-alias -g NOERR='2>/dev/null' # エラー非表示のグローバルエイリアス
+alias ll='ls -l'                     # 詳細情報表示
+alias la='ls -la'                    # 隠しファイルも表示
+alias -g NOERR='2>/dev/null'         # エラー非表示のグローバルエイリアス
+
+# dotfiles用git command
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'
